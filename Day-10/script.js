862c5e084b12db45f114a6bd284f5cdc8edf5f58
Nisarg Patel <nisargpatel6504@gmail.com>
2024-07-22 22:11:28 +0530

    More concise..

--- Day-10/script.js
+++ Day-10/script.js
@@ -1,13 +1,11 @@
 // Activity 1
-const btn1 = document.querySelector("#btn-1");
 const changeText = document.getElementById("changeText");
-btn1.addEventListener("click", () => {
+document.querySelector("#btn-1").addEventListener("click", () => {
   changeText.innerText = "This is new content..";
 });
-const div = document.querySelector("#toggleImg");
 const img = document.querySelector("#toggleImg>img");
 let flag = true;
-div.addEventListener("dblclick", () => {
+document.querySelector("#toggleImg").addEventListener("dblclick", () => {
   if (flag == true) {
     img.style.opacity = 0;
     flag = false;
@@ -27,25 +25,20 @@ toggleBg.addEventListener("mouseout", () => {
 });
 
 // Activity 3
-const inp1 = document.querySelector("#input-1");
-inp1.addEventListener("keydown", (e) => {
+document.querySelector("#input-1").addEventListener("keydown", (e) => {
   console.log(e.key);
 });
-const inp2 = document.querySelector("#input-2");
 const displatText = document.querySelector("#displatText");
-inp2.addEventListener("keyup", (e) => {
+document.querySelector("#input-2").addEventListener("keyup", (e) => {
   displatText.innerText = `Current value : ${inp2.value}`;
 });
 
 // Activity 4
-const form = document.querySelector("#userForm");
-const submitBtn = document.querySelector("#submitBtn");
-const city = document.querySelector("#city");
 const displayCity = document.querySelector("#displayCity");
-city.addEventListener("change", () => {
+document.querySelector("#city").addEventListener("change", () => {
   displayCity.innerText = `Selected city : ${city.value}`;
 });
-form.addEventListener("submit", (e) => {
+document.querySelector("#userForm").addEventListener("submit", (e) => {
   e.preventDefault();
   const formData = new FormData(form);
   formData.forEach((value, key) => {
@@ -54,20 +47,17 @@ form.addEventListener("submit", (e) => {
 });
 
 // Activity 5
-const list = document.querySelector("#list");
-list.addEventListener("click", (e) => {
+document.querySelector("#list").addEventListener("click", (e) => {
   if (e.target.tagName == "LI") {
     console.log(e.target.textContent);
   }
 });
-const parent = document.getElementById("parent");
-const addChildButton = document.getElementById("addChild");
-addChildButton.addEventListener("click", () => {
+document.getElementById("addChild").addEventListener("click", () => {
   const newChild = document.createElement("div");
   newChild.textContent = "New Child Element";
   parent.appendChild(newChild);
 });
-parent.addEventListener("click", (e) => {
+document.getElementById("parent").addEventListener("click", (e) => {
   if (e.target !== addChildButton) {
     console.log(e.target.textContent);
   }
